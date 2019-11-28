75a7f94d2944e89e108590754854da09a986f673
xiaoping <xiaoxiamivip@vip.qq.com>
2019-11-28 11:43:04 +0800

    edit login

--- logic/user/login.go
+++ logic/user/login.go
@@ -4,6 +4,7 @@ import (
 	"github.com/kataras/iris/v12"
 	//"github.com/kataras/iris/v12/mvc"
 	"fmt"
+	"github.com/go-playground/validator/v10"
 	"github.com/xiaoxiamivip/wechat/model/user"
 )
 
@@ -13,5 +14,7 @@ type LoginController struct {
 
 func (c *LoginController) GetIndexBy(test string, id int64) (int, error) {
 	user.GetUserInfo()
+	err := validate.Struct(mystruct)
+	validationErrors := err.(validator.ValidationErrors)
 	return c.Ctx.JSON(iris.Map{"message": test, "id": fmt.Sprintf("%d", id)})
 }
